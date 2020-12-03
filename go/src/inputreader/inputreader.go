bb7694c65c3bcefac7fb8634bf47087cc6df4681
Chris Soto <chris.soto@ckeefe-10p.fourwindsinteractive.com>
2020-12-03 13:07:25 -0700

    add go and put node into its own directory

---
+++ go/src/inputreader/inputreader.go
@@ -0,0 +1,34 @@
+package inpututils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+// ParseFile must be Exported, Capitalized, and comment added.
+func ParseFile(path string) []string {
+	// read file
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	// define data structure
+	type DayPrice struct {
+		USD float32
+		EUR float32
+		GBP float32
+	}
+
+	// json data
+	var obj []string
+
+	// unmarshall it
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return obj
+}
