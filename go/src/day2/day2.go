bedab2e0050c6e90bf958c284e5ca21867f7b78b
Chris Soto <chris.soto@ckeefe-10p.fourwindsinteractive.com>
2020-12-03 13:40:56 -0700

    fix the file parser. can now start working with GO

--- go/src/day2/day2.go
+++ go/src/day2/day2.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"inputreader"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	obj := inputreader.ParseFile("go/src/day2/input.json")
+	fmt.Println("OBJ: ", obj[0])
 }
--- go/src/inputreader/inputreader.go
+++ go/src/inputreader/inputreader.go
@@ -1,34 +1,27 @@
 package inputreader
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
 // ParseFile must be Exported, Capitalized, and comment added.
-func ParseFile(path string) {
-	// // read file
-	// data, err := ioutil.ReadFile(path)
-	// if err != nil {
-	// 	fmt.Print(err)
-	// }
+func ParseFile(path string) []string {
+	// read file
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Print(err)
+	}
 
-	// // define data structure
-	// type DayPrice struct {
-	// 	USD float32
-	// 	EUR float32
-	// 	GBP float32
-	// }
+	// json data
+	var obj []string
 
-	// // json data
-	// var obj []string
+	// unmarshall it
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
 
-	// // unmarshall it
-	// err = json.Unmarshal(data, &obj)
-	// if err != nil {
-	// 	fmt.Println("error:", err)
-	// }
-
-	// return obj
-
-	fmt.Println("Hello from input reader")
+	return obj
 }
