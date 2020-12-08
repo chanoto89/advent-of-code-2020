df25e1831a6b23ed183e01a64e27dde8186d9fd0
chanoto89 <csoto@mail.usf.edu>
2020-12-08 01:03:31 -0700

    some final refactoring

--- node/day6/index.ts
+++ node/day6/index.ts
@@ -1,34 +1,35 @@
+declare global {
+  interface Array<T> {
+    intersection(): Array<T>;
+  }
+}
+
 import InputReader from '../utils/InputReader';
 
-const calculateYesPerGroup = (input: string[]): number => {
+const part1 = (input: string[]): number => {
   return input.reduce((prev, curr) => {
     const distinct = Array.from(new Set(curr.replace(/\n/g, '').split(''))).join('');
     return prev + distinct.length;
   }, 0);
 };
 
-const calculateUniqueYesAnswersV2 = (input: string[]): number => {
-  return input.reduce((prev, group) => {
-    const members = group.split('\n');
-    prev += members.reduce((matches, currAnswer) => {
-      return stringIntersection(matches, currAnswer);
-    }).length;
-    return prev;
-  }, 0);
+const part2 = (input: string[]): number => {
+  return input.map(x => x.split('\n')).reduce((prev, curr) => { return prev + curr.intersection().length; }, 0);
 };
 
-const stringIntersection = (oldValue: string, newValue: string) => {
-  return (oldValue.match(new RegExp(`[${newValue}]`, 'g')) || []).join('');
+Array.prototype.intersection = function () {
+  return this.reduce((prev, curr) => {
+    return (prev.match(new RegExp(`[${curr}]`, 'g')) || []).join('');
+  });
 };
-
 
 const run = () => {
   console.log('Advent of Code Day 6');
   const textFile = InputReader.readTxtFile('../../day6/input.txt');
   const input = textFile.split('\n\n');
-  const part1YesAnswers = calculateYesPerGroup(input);
+  const part1YesAnswers = part1(input);
   console.log('Part 1 answer: ', part1YesAnswers);
-  const part2Answer = calculateUniqueYesAnswersV2(input);
+  const part2Answer = part2(input);
   console.log('Part 2 answer: ', part2Answer);
 };
 
